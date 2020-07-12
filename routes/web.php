455891fdb4dc6b85e9cc666ae6d3107799cec23e
gomiryo <gomiryo@gmail.com>
2020-07-12 21:35:44 +0900

    p38

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get("hello/{mes}", function ($mes) {
+Route::get("hello/{mes?}", function ($mes = "no message") {
 
 $html = <<<EOF
 <html>
